aed0afa669fc4538195e82163189b561f781b867
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2019-10-22 10:34:46 -0400

    added segmentation manifest

--- manifest_file_generator.py
+++ manifest_file_generator.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 
-def main(my_list, image_list):
+def main(my_list, image_list, manifest_type):
     if not os.path.isfile(my_list):
         print("File path {} does not exist. Exiting...".format(my_list))
         sys.exit()
@@ -12,7 +12,12 @@ def main(my_list, image_list):
         sys.exit()
 
     try:
-        print('studyid,clinicaltrialsubjectid,imageid,filename')
+        if manifest_type == 'segmentation':
+            print('segmentdir,studyid,clinicaltrialsubjectid,imageid')
+
+        if manifest_type == 'map':
+            print('studyid,clinicaltrialsubjectid,imageid,filename')
+
         with open(my_list) as fa:
             for line_a in fa:
                 with open(image_list) as fb:
@@ -21,7 +26,10 @@ def main(my_list, image_list):
                         y = line_b[:line_b.find(".")].strip()
                         row = line_b.strip().split(',')
                         if x in y:
-                            print(row[1] + "," + row[2] + "," + row[3] + "," + line_a.strip())
+                            if manifest_type == 'map':
+                                print(row[1] + "," + row[2] + "," + row[3] + "," + line_a.strip())
+                            if manifest_type == 'segmentation':
+                                print(line_a.strip() + "," + row[1] + "," + row[2] + "," + row[3])
                             continue
 
     except Exception as ex:
@@ -33,8 +41,8 @@ if __name__ == '__main__':
     # 1) Download httplinks.csv from PathDB
     # 2) REMOVE CSV HEADER!
     # 3) ls -l | awk '{print $9}' > ~/myList.list  # Make sure no blank lines or 'extra' files that don't belong.
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 4:
         print('\nUSAGE:\n    python ' + os.path.basename(
-            __file__) + ' /path/to/yourImageList.list /path/to/httplinks.csv')
+            __file__) + ' /path/to/yourImageList.list /path/to/httplinks.csv manifest_type [map | segmentation]')
     else:
-        main(sys.argv[1], sys.argv[2])
+        main(sys.argv[1], sys.argv[2], sys.argv[3])
